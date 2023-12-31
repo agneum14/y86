1e0ab462e5e3a3e7b85e92bd9b04300ea9395f88
Andrew G. Neumann <agneum14@gmail.com>
2023-12-31 18:29:04 -0500

    read the header

---
+++ src/interp.rs
@@ -0,0 +1,73 @@
+use std::io::Cursor;
+
+use binrw::BinRead;
+
+const MAGIC: u32 = 0x464c45;
+
+#[derive(BinRead)]
+pub struct ElfHdr {
+    version: u16,
+    entry: u16,
+    phdr_start: u16,
+    num_phdr: u16,
+    symtab: u16,
+    strtab: u16,
+    magic: u32,
+}
+
+pub fn read_header(reader: &mut Cursor<Vec<u8>>) -> Option<ElfHdr> {
+    let hdr = match ElfHdr::read_le(reader) {
+        Ok(v) => v,
+        Err(_) => return None,
+    };
+
+    if hdr.magic != MAGIC {
+        return None;
+    }
+    Some(hdr)
+}
+
+trait Stringify {
+    fn stringify(&self) -> String;
+}
+
+impl Stringify for u16 {
+    fn stringify(&self) -> String {
+        format!("{:02x} {:02x}", self & 0x00FF, self >> 8)
+    }
+}
+pub fn dump_header(hdr: &ElfHdr) {
+    println!(
+        "{} {} {} {}  {} {} 45 4c 46 00",
+        hdr.version.stringify(),
+        hdr.entry.stringify(),
+        hdr.phdr_start.stringify(),
+        hdr.num_phdr.stringify(),
+        hdr.symtab.stringify(),
+        hdr.strtab.stringify()
+    );
+    println!("Mini-ELF version {}", hdr.version);
+    println!("Entry point 0x{:x}", hdr.entry);
+    println!(
+        "There are {} program headers, starting at offset {} (0x{:x})",
+        hdr.num_phdr, hdr.phdr_start, hdr.phdr_start
+    );
+
+    if hdr.symtab == 0 {
+        println!("There is no symbol table present");
+    } else {
+        println!(
+            "There is a symbol table starting at offset {} (0x{:x})",
+            hdr.symtab, hdr.symtab
+        );
+    }
+
+    if hdr.strtab == 0 {
+        println!("There is no string table present");
+    } else {
+        println!(
+            "There is a string table starting at offset {} (0x{:x})",
+            hdr.strtab, hdr.strtab
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::{io::Cursor, process::exit};
+
+use clap::Parser;
+use interp::{read_header, dump_header};
+
+mod interp;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Show the Mini-ELF header
+    #[arg(short = 'H')]
+    print_hdr: bool,
+
+    /// Mini-ELF object file
+    file: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let bytes = match std::fs::read(args.file) {
+        Ok(v) => v,
+        Err(_) => {
+            println!("Failed to read file");
+            exit(1);
+        }
+    };
+    let mut reader = Cursor::new(bytes);
+
+    let hdr = match read_header(&mut reader) {
+        Some(v) => v,
+        None => {
+            println!("Failed to read file");
+            exit(2);
+        }
+    };
+
+    if args.print_hdr {
+        dump_header(&hdr);
+    }
 }
